7bec6026754835abaffcd6154880920315b8709d
skylar <skylar.saveland@mycogia.com>
2009-10-05 22:39:58 +0000

    example local settings

--- empty_local_settings.py
+++ empty_local_settings.py
@@ -1,38 +1,57 @@
-DEBUG = True
-SERVE_MEDIA = True
-#DATABASE_NAME =
-#DATABASE_USER =
-#DATABASE_PASSWORD =
-#DATABASE_HOST =
-#DATABASE_PORT =
+if SERVE_MEDIA:
+    # this is the one for 127.0.0.1:8000
+    GOOGLE_API_KEY = ''
+else:
+    GOOGLE_API_KEY = ''
+
+BEHIND_PROXY = True
+
+ADMINS = (
+    ('you', 'you@gmail.com'),
+)
+SERVE_MEDIA = False
+
+DEBUG = False
+SERVE_MEDIA = False
+DATABASE_NAME = ''
+DATABASE_USER = ''             # Not used with sqlite3.
+DATABASE_PASSWORD = ''         # Not used with sqlite3.
+DATABASE_HOST = ''             # Set to empty string for localhost. Not used with sqlite3.
+DATABASE_PORT = ''             # Set to empty string for default. Not used with
 
 
-#EMAIL_HOST =
-#EMAIL_HOST_USER =
-#EMAIL_HOST_PASSWORD =
-#EMAIL_PORT =
-#EMAIL_USE_TLS =
-#DEFAULT_FROM_EMAIL =
+EMAIL_HOST = ''
+EMAIL_HOST_USER = ''
+EMAIL_HOST_PASSWORD = ''
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
+DEFAULT_FROM_EMAIL = ''
 
-#DUMMY_FACEBOOK_INFO = {
-#    'uid':0,
-#    'name':'(Private)',
-#    'first_name':'(Private)',
-#    'pic_square_with_logo':'/public/images/t_silhouette.jpg',
-#    'affiliations':'server goof',
-#    'status':'facebook messed up',
-#    'proxied_email':None,
-#}
+DUMMY_FACEBOOK_INFO = {
+    'uid':0,
+    'name':'(Private)',
+    'first_name':'(Private)',
+    'pic_square_with_logo':'/public/images/t_silhouette.jpg',
+    'affiliations':'server goof',
+    'status':'facebook messed up',
+    'proxied_email':None,
+}
+# domain.com
+FACEBOOK_API_KEY = ''
+FACEBOOK_SECRET_KEY = ''
 
-#FACEBOOK_API_KEY =
-#FACEBOOK_SECRET_KEY =
-#FACEBOOK_INTERNAL =
+# local development
+#FACEBOOK_API_KEY = ''
+#FACEBOOK_SECRET_KEY = ''
+FACEBOOK_INTERNAL = True
 
-#BBAUTH_APP_ID =
-#BBAUTH_SHARED_SECRET =
+BBAUTH_APP_ID = ''
+BBAUTH_SHARED_SECRET = ''
 
-#MEDIA_URL =
-#ADMIN_MEDIA_PREFIX =
 
-#SECRET_KEY =
+MEDIA_URL = ''
+ADMIN_MEDIA_PREFIX = ''
+
+SECRET_KEY = ''
+
 
