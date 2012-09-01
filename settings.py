ac0d935a1e89b846abe836abb6819bc4a2560f1f
Skylar Saveland <skylar.saveland@gmail.com>
2012-09-01 12:11:19 -0400

    moved to django-social-auth with fb and twitter

--- settings.py
+++ settings.py
@@ -171,7 +171,7 @@ INSTALLED_APPS = (
 
 
 AUTHENTICATION_BACKENDS = (
-    #'social_auth.backends.twitter.TwitterBackend',
+    'social_auth.backends.twitter.TwitterBackend',
     'social_auth.backends.facebook.FacebookBackend',
     #'social_auth.backends.google.GoogleOAuthBackend',
     #'social_auth.backends.google.GoogleOAuth2Backend',
